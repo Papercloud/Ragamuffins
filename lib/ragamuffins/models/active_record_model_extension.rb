db14d3cf862638227e98c462663e3cf9b2277094
Tomas Spacek <tomspacek@gmail.com>
2014-05-23 01:10:03 +1000

    Strip out eval as it was causing FavouriteObjec::Favourite to represent its table name as 'favourites'

--- lib/ragamuffins/models/active_record_model_extension.rb
+++ lib/ragamuffins/models/active_record_model_extension.rb
@@ -3,17 +3,14 @@ module Ragamuffins
     extend ActiveSupport::Concern
 
     included do
-      # self.send(:include, Ragamuffins::ConfigurationMethods)
 
       # Fetch all the deleted ids from the backend, and return them as an array
-      #   Model.delted_ids = []
-      eval <<-RUBY
-        def self.show_deleted_ids(ids = [])
-          return [] if ids == nil
+      #   Model.deleted_ids = []
+      def self.show_deleted_ids(ids = [])
+        return [] if ids == nil
 
-          ids.collect{|s| s.to_i} - self.where("#{table_name}.id IN (?)", ids).map(&:id)
-        end
-      RUBY
+        ids.collect{|s| s.to_i} - self.where("#{table_name}.id IN (?)", ids).map(&:id)
+      end
     end
   end
 end
